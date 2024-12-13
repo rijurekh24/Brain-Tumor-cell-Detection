17a5c7619f4964902d563b20c16edf3c82f962eb
rijurekh24 <rijurekhghosh2017@gmail.com>
2024-12-13 22:09:16 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Signin from "./Pages/Signin";
 import SignUp from "./Pages/SignUp";
+import NotFound from "./Pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/signin" element={<Signin />} />
         <Route path="/signup" element={<SignUp />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
   );
